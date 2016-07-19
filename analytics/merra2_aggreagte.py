6bc8e94600aa9871058afefe553402547ec4fbea
Nithin Krishna <nithinkrishh@gmail.com>
2016-07-18 23:13:27 -0400

    total smc

--- analytics/merra2_aggreagte.py
+++ analytics/merra2_aggreagte.py
@@ -14,13 +14,32 @@ df = pd.read_csv(PATH, names=[
   'RegionId', 'Year', 'Month', 'Latitude', 'Longitude', 'BCSCATAU',
   'BCSMASS', 'OCSCATAU', 'OCSMASS', 'DUSCATAU', 'DUSMASS', 'SSSCATAU',
   'SSSMASS', 'SUSCATAU', 'SO4SMASS', 'SO2SMASS', 'TOTSCATAU'
-], index_col=False)
+], index_col=False, dtype = {
+  'RegionId' : np.int64,
+  'Year' : np.int64,
+  'Month' : np.str,
+  'Latitude' : np.float64,
+  'Longitude' : np.float64,
+  'BCSCATAU' : np.float64,
+  'BCSMASS' : np.float64,
+  'OCSCATAU' : np.float64,
+  'OCSMASS' : np.float64,
+  'DUSCATAU' : np.float64,
+  'DUSMASS' : np.float64,
+  'SSSCATAU' : np.float64,
+  'SSSMASS' : np.float64,
+  'SUSCATAU' : np.float64,
+  'SO4SMASS' : np.float64,
+  'SO2SMASS' : np.float64,
+  'TOTSCATAU' : np.float64,
+})
 
+df['TOTALSMC'] = df['BCSMASS'] + df['OCSMASS'] + df['DUSMASS'] + df['SSSMASS'] + df['SO4SMASS'] + df['SO2SMASS']
 
 g = df[[
   'RegionId', 'Year', 'BCSCATAU','BCSMASS', 'OCSCATAU',
   'OCSMASS', 'DUSCATAU', 'DUSMASS', 'SSSCATAU', 'SSSMASS',
-  'SUSCATAU', 'SO4SMASS', 'SO2SMASS', 'TOTSCATAU'
+  'SUSCATAU', 'SO4SMASS', 'SO2SMASS', 'TOTSCATAU', 'TOTALSMC'
 ]].groupby(['RegionId', 'Year'], as_index=False).agg([ np.min, np.max, np.sum, np.mean, np.median ])
 
 # Write aggregate to csv
